3acf1f7d70c6e41240338d727bac71134b83188d
Yana Vovk <yanavovk1990@outlook.com>
2021-09-10 06:52:03 +0300

    Fix hooks usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import useFeedback from './hooks/useFeedback';
 import Feedback from './components/Feedback/FeedbackOptions';
 import Statistics from './components/Statistics/Statistics';
 import Cover from './components/Cover/Cover';
@@ -10,32 +9,27 @@ export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(-1);
 
-  const onLeaveFeedback = useFeedback();
+  const onLeaveFeedback = (e) => {
+      if (e.target.name === 'good') {
+        setGood(good => good + 1);
+        }
+        if (e.target.name === 'neutral') {
+             setNeutral(neutral=> neutral +1);
+         }
+        if (e.target.name === 'bad') {
+             setBad(bad=> bad +1);
+         }
+  }
   const options = ['good','neutral','bad'];
 
-  //  const onLeaveFeedback = (e) => {
-  //   const grade = e.target.name;
-  //   setGood((currentValue) => ({
-  //     [grade]: currentValue[grade] + 1,
-  //   }))
-  // };
-
-  useEffect(() => { setTotal(state => state + 1);},[good,neutral,bad])
-
-  // const countTotalFeedback = () => {
-  //   return Object.values(state).reduce((acc, value) => (acc += value), 0);
-  // };
+  useEffect(() => { setTotal(total => total + 1); }, [good, neutral, bad]); 
   
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100)/total);
+  }
 
-  
-  // countPositiveFeedbackPercentage = (good, total) => {
-  //   return Math.round((good * 100)/total);
-  // }
-
-    // const positivePercentage = this.countPositiveFeedbackPercentage(this.state.good, total);
-  
   return (
     <div>
       <Cover
@@ -44,7 +38,7 @@ export default function App() {
           options={options}
           onLeaveFeedback={onLeaveFeedback} />
       </Cover>
-      {/* <Cover
+      <Cover
         title="Statistics">
         {total > 0
           ? (<Statistics
@@ -52,10 +46,10 @@ export default function App() {
             neutral={neutral}
             bad={bad}
             total={total}
-            positivePercentage={positivePercentage} />)
+            positivePercentage={countPositiveFeedbackPercentage()} />)
           : (<Notification message="No feedback given" />)
         }
-      </Cover> */}
+      </Cover>
     </div>
   );
 }
